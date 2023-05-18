3336f3636902589b7216a637baf40b745cf4b184
Brioni Connolly <brioniconnolly@gmail.com>
2023-05-18 20:37:54 +0000

    add division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,11 +45,14 @@ function runGame(gameType) {
     displayMultiplyQuestion(num1, num2);
    } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
+   } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
    } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
  }
 }
+
 /**
  * Checks the answer against the first element in the returned 
  * calculatedCorrectAnswer array
@@ -85,14 +88,13 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];}
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
-
 }
-
-
 /**
  * Gets the current score from the dom and increments it by 1
  */
@@ -132,5 +134,14 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    operand1 = operand1 * operand2;
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
 
